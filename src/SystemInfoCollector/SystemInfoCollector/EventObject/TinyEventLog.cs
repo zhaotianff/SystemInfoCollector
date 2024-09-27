6d92cc0b6095b82ccade3965f513df2cf04a8fb5
zhaotianff <zhaotianff@163.com>
2024-09-27 17:34:27 +0800

    add event log model

---
+++ src/SystemInfoCollector/SystemInfoCollector/EventObject/TinyEventLog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TianXiaTech.SystemInfoCollector.EventObject
+{
+    public class TinyEventLog
+    {
+        public string Source { get; private set; }
+
+        public string Log { get; private set; }
+
+        public List<TinyEventLogEntry> Entries { get; private set; }
+
+        public string LogDisplayName { get; private set; }
+
+        public string MachineName { get; private set; }
+
+        public long MaximumKilobytes { get; private set; }
+    }
+}
---
+++ src/SystemInfoCollector/SystemInfoCollector/EventObject/TinyEventLogEntry.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Text;
+
+namespace TianXiaTech.SystemInfoCollector.EventObject
+{
+    public class TinyEventLogEntry
+    {
+        public string Category { get; private set; }
+
+        public short CategoryNumber { get; private set; }
+
+        public byte[] Data { get; private set; }
+
+        public EventLogEntryType EntryType { get; private set; }
+
+        public int Index { get; private set; }
+
+        public long InstanceId { get; private set; }
+
+        public string MachineName { get; private set; }
+
+        public string Message { get; private set; }
+
+        public string[] ReplacementStrings { get; private set; }
+
+        public string Source { get; private set; }
+
+        public DateTime TimeGenerated { get; private set; }
+
+        public DateTime TimeWritten { get; private set; }
+
+        public string UserName { get; private set; }
+    }
+}
